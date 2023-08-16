c95bc69297a1918cc1e95b3a9fd11e2ba6732003
COMMON-PC\Common <shypulin@ukr.net>
2023-08-17 00:36:08 +0300

    Initial migrations are made

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+# class Estate(models.Model):
+#     pass
+# BooleanField
+# CharField
+# CharField.max_length
+# TextField
+# DateField
+# DateField.auto_now
+# EmailField
+# FilePathField
+# FloatField
+# ImageField
+# IntegerField
+#
+# ForeignKey
+# Чтобы создать рекурсивное отношение - объект, который имеет отношение «многие-к-одному» с самим
+# собой - используйте models.ForeignKey('self', on_delete = models.CASCADE)
+# ForeignKey.on_delete
+# ManyToManyField
+# OneToOneField
